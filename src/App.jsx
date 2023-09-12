c60498d15c1775e424cda2cb52149fd4b1d85f5e
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-12 14:41:03 +0300

    Creating a movies site

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import search from './assets/search.png'
+import MovieCard from './movieCard'
+
+function App() {
+  const API_URL = 'http://www.omdbapi.com/?apikey=3f609f6a'
+
+  const [movies, setMovies] = useState([]) // movies is the state variable and setMovies is the function that updates the state variable
+
+  const [searchTerm, setSearchTerm] = useState('') // searchTerm is the state variable and setSearchTerm is the function that updates the state variable
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`); // fetch data from API
+    const data = await response.json(); // convert data to JSON
+    setMovies(data.Search); // update state variable
+  }
+
+  useEffect(() => {
+    searchMovies('Harry Potter');
+  }, [])
+  return (
+    <>
+      <div className="app">
+        <h1>Movie Quest<br /><span>by Eclar Khalid</span></h1>
+        <div>
+        <h3>Search by title, genre, or actors to discover information like year, cast, and Movie title. It's your one-stop movie resource.</h3>
+        </div>
+
+        <div className="search">
+          <input type="text" placeholder='Search for your favourite movie'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value) }
+          />
+          <img src={search} alt="search icon" onClick={() => searchMovies(searchTerm)} />
+        </div>
+
+        {
+        movies?.length > 0 ? (
+            <div className="container">
+              {
+                movies.map((movie) => (
+                  <MovieCard movie={movie} />
+                ))
+              }
+
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies to show</h2>
+            </div>
+          )
+        }
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
